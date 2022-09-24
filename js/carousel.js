60fa7930fb211a7cdc866f731fd2accb87127686
MaxVoices <38167382+MaxVocies@users.noreply.github.com>
2022-09-24 16:05:02 +0300

    Start

---
+++ js/carousel.js
@@ -0,0 +1,18 @@
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.next').addEventListener('click', function(){
+  offset = offset + 100;
+  if (offset == 300) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + '%';
+});
+
+document.querySelector('.prev').addEventListener('click', function(){
+  offset = offset - 100;
+  if (offset < 0) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + '%';
+});
